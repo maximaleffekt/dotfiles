cb93b5c63fa2ecd50c8da6d46973c8cbb5f37776
MasterMax13124 <mastermax13124@gmail.com>
2021-05-23 11:49:50 +0200

    Better volume/mute indicator

--- Arch/dwm/dwmscripts/xsetroot-script-thinkpad.sh
+++ Arch/dwm/dwmscripts/xsetroot-script-thinkpad.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
 while true; do
-	xsetroot -name " 1:$(acpi | head -n 1 | cut -d ',' -f2) |  2:$(acpi | tail -n 1 | cut -d ',' -f2) |  $(xbacklight -get | cut -d. -f1)% |  $(pactl list sinks | grep '^[[:space:]]Volume:' | head -n 1 | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')% 婢 $(pactl list sinks | grep -E "Sink " -A 8 | awk '/Mute: / {print $2}' | head -1) | $( date +"%F %R" )"
+	var1=$(pactl list sinks | grep -E "Sink " -A 8 | awk '/Mute: / {print $2}' | head -1)
+	xsetroot -name " 1:$(acpi | head -n 1 | cut -d ',' -f2) |  2:$(acpi | tail -n 1 | cut -d ',' -f2) |  $(xbacklight -get | cut -d. -f1)% | $(if [[ "$var1" == 'yes' ]]; then echo 婢; else echo 墳; fi) $(pactl list sinks | grep '^[[:space:]]Volume:' | head -n 1 | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')% | $( date +"%F %R" )"
 	sleep 0.5s
 done
 # set the first head value either to one or two, change the last word in line three either to head or tail respectively, can change depending on device order
